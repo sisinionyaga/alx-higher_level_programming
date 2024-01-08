e1298af7f30a076990192edc76a1f9a04fea9d77
sisinionyaga <sysynyo@gmail.com>
2024-01-08 01:37:52 -0800

    my additions in py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,15 @@ if __name__ == "__main__":
     total = 0
     for i in range(len(sys.argv) - 1):
         total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    print("{}".format(total)#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the addition of all arguments."""
+    import sys
+
+    # Convert command-line arguments to integers and sum them
+    total = sum(map(int, sys.argv[1:]))
+
+    # Print the result followed by a new line
+    print(total)
+)
